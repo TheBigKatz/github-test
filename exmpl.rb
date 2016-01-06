cc73351e4f036c08c0eace3b24d1f2aa3a45a3b1
Andrew Katz <andrewkz123@yahoo.com>
2016-01-05 18:27:32 -0600

    Fav Food In Order

--- exmpl.rb
+++ exmpl.rb
@@ -4,6 +4,9 @@ puts "Write down your top 5 favorite foods!"
 	food = gets.chomp
 	favorite_foods << food
 end 
+counter = 1
 favorite_foods.each do |food|
-	p "I love #{food}"
-end
+	puts "#{counter}. #{food}"
+	counter +=1 
+	end
+   
