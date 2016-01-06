4468f94eae8b3a537dffa80b637083d8e32fc560
Andrew Katz <andrewkz123@yahoo.com>
2016-01-05 18:57:05 -0600

    Hello

---
+++ discussion.rb
@@ -0,0 +1,10 @@
+# if answer != 5
+# What this code is doing is determining where or not if the answer is equal to 5"
+
+# if result != true
+# This is determining if the answer is true or faslse
+
+# unless result == true
+
+unless result == true 
+	puts "hello"
---
+++ hello.rb
@@ -0,0 +1,6 @@
+# Look at the conditional below. What value(s) can the variable ‘result’ be for it to output “HELLO!” What values will it not output? Experiment in Sublime:
+result = true 
+
+unless result 
+  puts "HELLO!"
+end 
