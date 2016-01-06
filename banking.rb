7ed075df759ddb3abafc67cd1f1146a56a396eec
Andrew Katz <andrewkz123@yahoo.com>
2016-01-05 20:41:15 -0600

    Banking Software

--- banking.rb
+++ banking.rb
@@ -5,14 +5,23 @@ puts "******************************"
 
 accounts = []
 
-5.times do 
+2.times do 
 	print "Enter account holders First Name: "
 	first_name = gets.chomp
 
 	print "Enter account holders Last Name: "
 	last_name = gets.chomp
 
-	accounts << {first_name: first_name, last_name: last_name}
+	print "Enter account holders Email: "
+	email = gets.chomp
+
+	acct_num = (1000000000..9999999999)
+
+	puts ""
+	puts "****************************"
+	puts ""
+
+	accounts << {first_name: first_name, last_name: last_name, email: email}
 
 end
 
