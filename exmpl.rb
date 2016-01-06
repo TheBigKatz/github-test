0199c532158b89b73592cb8e27192894ad1cccb5
Andrew Katz <andrewkz123@yahoo.com>
2016-01-05 18:22:01 -0600

    Favorite Foods

--- exmpl.rb
+++ exmpl.rb
@@ -1,7 +1,9 @@
 favorite_foods = []
-
+puts "Write down your top 5 favorite foods!"
 5.times do |food|
 	food = gets.chomp
 	favorite_foods << food
 end 
-p favorite_foods 
+favorite_foods.each do |food|
+	p "I love #{food}"
+end
